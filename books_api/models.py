b8debc4e551da5eb143f45ffb10f953654fc58fd
Owen Klan <owen.j.klan@gmail.com>
2024-06-20 11:44:58 +1000

    Initial models and migration script

--- books_api/models.py
+++ books_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class BookFormatChoices(models.TextChoices):
+    HARDBACK = "Hard Cover"
+    PAPERBACK = "Paperback"
+    EBOOK = "Ebook"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    format = models.CharField(max_length=25, choices=BookFormatChoices.choices)
+    rrp = models.DecimalField(max_digits=5, decimal_places=2)
+    publisher = models.ForeignKey("Publisher", on_delete=models.CASCADE)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    year_of_birth = models.PositiveSmallIntegerField()
+    year_of_death = models.PositiveSmallIntegerField(null=True, blank=True)
+    books = models.ManyToManyField("Book", related_name="authors")
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField("Book", related_name="categories")
