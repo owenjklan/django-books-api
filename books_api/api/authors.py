9ff44d3dc1300c76498b8aac092e373f09778605
Owen Klan <owen.j.klan@gmail.com>
2024-06-20 13:28:46 +1000

    Add basic blumbing for Author retrieval

--- books_api/api/authors.py
+++ books_api/api/authors.py
@@ -1,15 +1,19 @@
 from django.http import JsonResponse, HttpRequest
 from ninja import Router
 
+from ..models import Author
+from ..schemas import AuthorSchema
 
 router = Router(tags=["Authors"])
 
 
-@router.get("/")
+@router.get("/", response=list[AuthorSchema])
 def get_authors(request: HttpRequest) -> JsonResponse:
-    return "GET Authors"
+    authors = Author.objects.all()
+    return authors
 
 
-@router.get("/{int:author_id}")
+@router.get("/{int:author_id}", response=AuthorSchema)
 def get_author(request: HttpRequest, author_id: int) -> JsonResponse:
-    return JsonResponse({"author_id": author_id})
+    author = Author.objects.get(pk=author_id)
+    return author
--- books_api/schemas.py
+++ books_api/schemas.py
@@ -1,11 +1,18 @@
 from ninja import Schema, ModelSchema
 
-from books_api.models import Book, Publisher
+from books_api.models import Book, Publisher, Author
 
 
 class BookSchema(ModelSchema):
     publisher: "PublisherSchema"
+    authors: list["AuthorSubSchema"]
 
+    class Config:
+        model = Book
+        model_fields = "__all__"
+
+
+class BookSubSchema(ModelSchema):
     class Config:
         model = Book
         model_fields = "__all__"
@@ -15,3 +22,31 @@ class PublisherSchema(ModelSchema):
     class Config:
         model = Publisher
         model_fields = "__all__"
+
+
+class AuthorSchema(ModelSchema):
+    """
+    This schema is used when the Author object is the main
+    object being queried
+    """
+
+    books: list["BookSubSchema"]
+
+    class Config:
+        model = Author
+        model_fields = "__all__"
+
+
+class AuthorSubSchema(ModelSchema):
+    """
+    This schema is used when Author information is provided
+    as additional, related information for a parent object's
+    Schema. For example, when authors are returned as part
+    of a book's detail, we don't need "books" listed again
+    as part of the "authors" key.
+    """
+
+    class Config:
+        model = Author
+        model_fields = "__all__"
+        model_exclude = ["books"]
